e0e66c848c36919ce2d12f9d5106e0d6d4967b61
Atanas Latinov <atanas.latinov@yahoo.com>
2020-02-07 14:56:49 +0100

    Assignment 1
    Assignment 1 commit

--- Assignment_1/Assignment_1.sql
+++ Assignment_1/Assignment_1.sql
@@ -7,6 +7,40 @@ DROP TABLE IF EXISTS `member`;
 
 SET FOREIGN_KEY_CHECKS=ON;
 
+CREATE TABLE `member` (
+    `member_number` INT,
+    `name` VARCHAR(64) NOT NULL,
+    `phone_number` VARCHAR(32) UNIQUE,
+    `email` VARCHAR(64) NOT NULL UNIQUE,
+    `address` VARCHAR(255),
+    PRIMARY KEY (`member_number`)
+) ENGINE=INNODB;
+
+CREATE TABLE `wine`(
+    `name` varchar(16),
+    `appellation` varchar(16),
+    `vintage` year,
+    `alcohol_degree` decimal check (`alcohol_degree` > 0 and `alcohol_degree` < 20),
+    `country` varchar(32),
+    `grape_variety` varchar(32),
+    primary key(`name`, `appellation`, `vintage`)
+) ENGINE=INNODB;
+
+CREATE TABLE `bottle`(
+	`wine_name` varchar(16) not null,
+    `wine_appellation` varchar(16) not null,
+    `wine_vintage` year not null,
+    `bottle_number` tinyint not null,
+    `status` bit not null,
+    primary key(`wine_name`, `wine_appellation`, `wine_vintage`, `bottle_number`)
+) ENGINE=INNODB;
+
+ALTER TABLE `bottle` 
+	ADD FOREIGN KEY (`wine_name`, `wine_appellation`,`wine_vintage`)
+	REFERENCES `wine`(`name`,`appellation`,`vintage`)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE;
+    
 CREATE TABLE `tasting_evaluation`(
     `bottle_name` varchar(16) not null,
     `bottle_appellation` varchar(16) not null,
@@ -22,43 +56,14 @@ CREATE TABLE `tasting_evaluation`(
     primary key(`bottle_name`,`bottle_appellation`, `bottle_vintage`, `bottle_number`, `evaluator`, `week`)
 ) ENGINE=INNODB ;
 
-
-CREATE TABLE `bottle`(
-	`wine_name` varchar(16) not null,
-    `wine_appellation` varchar(16) not null,
-    `wine_vintage` year not null,
-    `bottle_number` tinyint not null,
-    `status` bit not null,
-    primary key(`wine_name`, `wine_appellation`, `wine_vintage`, `bottle_number`)
-) ENGINE=INNODB;
-
-ALTER TABLE `bottle` 
-	ADD FOREIGN KEY (`wine_name`, `wine_appellation`,`wine_vintage`,`bottle_number`)
-	REFERENCES `tasting_evaluation`(`bottle_name`,`bottle_appellation`,`bottle_vintage`,`bottle_number`)
+ALTER TABLE `tasting_evaluation` 
+	ADD FOREIGN KEY (`bottle_name`, `bottle_appellation`,`bottle_vintage`,`bottle_number`)
+	REFERENCES `bottle`(`wine_name`,`wine_appellation`,`wine_vintage`,`bottle_number`)
     ON UPDATE CASCADE
     ON DELETE CASCADE;
-
-CREATE TABLE `wine`(
-    `name` varchar(16),
-    `appellation` varchar(16),
-    `vintage` year,
-    `alcohol_degree` decimal check (`alcohol_degree` > 0 and `alcohol_degree` < 20),
-    `country` varchar(32),
-    `grape_variety` varchar(32),
-    primary key(`name`, `appellation`, `vintage`)
-) ENGINE=INNODB;
-
-ALTER TABLE `wine` 
-	ADD FOREIGN KEY (`name`, `appellation`,`vintage`)
-	REFERENCES `bottle`(`wine_name`,`wine_appellation`,`wine_vintage`)
+    
+ALTER TABLE `tasting_evaluation` 
+	ADD FOREIGN KEY (`evaluator`)
+	REFERENCES `member`(`member_number`)
     ON UPDATE CASCADE
     ON DELETE CASCADE;
-
-CREATE TABLE `member`(
-    `member_number` int REFERENCES `tasting_evaluation`(`evaluator`),
-    `name` varchar(64) not null,
-    `phone_number` varchar(32) unique,
-    `email` varchar(64) not null unique,
-    `address` varchar(255),
-	primary key(`member_number`)
-) ENGINE=INNODB;
