80070f40dbfd349290449164deba2a4686959a3d
Atanas Latinov <atanas.latinov@yahoo.com>
2020-02-18 22:14:40 +0100

    Fixes and additions for assignment 2

--- Assignment_0/Assignment_0.sql
+++ Assignment_0/Assignment_0.sql
@@ -5,67 +5,67 @@ DROP TABLE IF EXISTS sells;
 
 SET FOREIGN_KEY_CHECKS=ON;
 
-create table `Beverage` (
+create table `beverage` (
 	`code` int,
     `name` varchar(15),
     `size` varchar(10),
     primary key(`code`)
 );
 
-INSERT INTO Beverage values(121, 'Latte', 'medium');
-INSERT INTO Beverage values(140, 'Latte', 'large');
+INSERT INTO beverage values(121, 'Latte', 'medium');
+INSERT INTO beverage values(140, 'Latte', 'large');
 
-INSERT INTO Beverage values(122, 'Tea', 'large');
-INSERT INTO Beverage values(131, 'Tea', 'small');
+INSERT INTO beverage values(122, 'Tea', 'large');
+INSERT INTO beverage values(131, 'Tea', 'small');
 
-INSERT INTO Beverage values(123, 'Cola', 'medium');
-INSERT INTO Beverage values(133, 'Cola', 'large');
+INSERT INTO beverage values(123, 'Cola', 'medium');
+INSERT INTO beverage values(133, 'Cola', 'large');
 
-INSERT INTO Beverage values(124, 'Mocha', 'medium');
-INSERT INTO Beverage values(134, 'Mocha', 'large');
+INSERT INTO beverage values(124, 'Mocha', 'medium');
+INSERT INTO beverage values(134, 'Mocha', 'large');
 
-INSERT INTO Beverage values(125, 'Pepsi', 'large');
-INSERT INTO Beverage values(135, 'Pepsi', 'medium');
+INSERT INTO beverage values(125, 'Pepsi', 'large');
+INSERT INTO beverage values(135, 'Pepsi', 'medium');
 
-INSERT INTO Beverage values(126, 'Schweppes', 'small');
-INSERT INTO Beverage values(136, 'Schweppes', 'medium');
+INSERT INTO beverage values(126, 'Schweppes', 'small');
+INSERT INTO beverage values(136, 'Schweppes', 'medium');
 
-INSERT INTO Beverage values(127, 'Dr Pepper', 'medium');
+INSERT INTO beverage values(127, 'Dr Pepper', 'medium');
 
-INSERT INTO Beverage values(128, 'Fanta', 'large');
+INSERT INTO beverage values(128, 'Fanta', 'large');
 
-INSERT INTO Beverage values(129, 'Sprite', 'medium');
+INSERT INTO beverage values(129, 'Sprite', 'medium');
 
-INSERT INTO Beverage values(130, 'Cappy', 'small');
+INSERT INTO beverage values(130, 'Cappy', 'small');
 
-INSERT INTO Beverage values(132, 'Mountain Dew', 'large');
+INSERT INTO beverage values(132, 'Mountain Dew', 'large');
 
-INSERT INTO Beverage values(137, 'Nestea', 'medium');
+INSERT INTO beverage values(137, 'Nestea', 'medium');
 
-INSERT INTO Beverage values(138, 'Lemonade', 'small');
+INSERT INTO beverage values(138, 'Lemonade', 'small');
 
-INSERT INTO Beverage values(139, 'Budweiser', 'medium');
+INSERT INTO beverage values(139, 'Budweiser', 'medium');
 
 /* ---------------------------------------------------- */
 
-create table `Store` (
-	`name` varchar(15),
+create table `store` (
+	`name` varchar(15) not null,
     `location` varchar (20),
     `telephone` numeric,
     primary key (`name`)
 );
 
-INSERT INTO Store values('REMA', 'Aarhus N', '45252163');
-INSERT INTO Store values('Fotex', 'Aarhus C', '45252153');
-INSERT INTO Store values('Bilka', 'Aarhus O', '45784163');
-INSERT INTO Store values('Netto', 'Aarhus V', '45284263');
-INSERT INTO Store values('Fakta', 'Aarhus V', '45215163');
+INSERT INTO store values('REMA', 'Horsens', '45252163');
+INSERT INTO store values('Fotex', 'Aarhus C', '45252153');
+INSERT INTO store values('Bilka', 'Randers', '45784163');
+INSERT INTO store values('Netto', 'Aarhus V', '45284263');
+INSERT INTO store values('Fakta', 'Horsens', '45215163');
 
 /* ---------------------------------------------------- */
 
 create table sells (
 	`store_name` varchar(15),
-    `code` varchar (20),
+    `code` int,
     `price` numeric,
     primary key (`store_name`, `code`)
 );
@@ -76,54 +76,54 @@ ALTER TABLE sells
 Alter table sells
 	add foreign key (`code`) references beverage(`code`);
 
-INSERT INTO Sells values('REMA', 127, '15');
-INSERT INTO Sells values('Fotex', 128, '20');
-INSERT INTO Sells values('REMA', 127, '15');
-INSERT INTO Sells values('Fotex', 129, '20');
-INSERT INTO Sells values('REMA', 130, '15');
-INSERT INTO Sells values('Fotex', 131, '20');
-INSERT INTO Sells values('Bilka', 140, '15');
-INSERT INTO Sells values('Netto', 139, '20');
-INSERT INTO Sells values('REMA', 125, '15');
-INSERT INTO Sells values('Fotex', 127, '20');
-INSERT INTO Sells values('REMA', 127, '15');
-INSERT INTO Sells values('Netto', 128, '20');
-INSERT INTO Sells values('Netto', 127, '15');
-INSERT INTO Sells values('Fotex', 131, '20');
-INSERT INTO Sells values('REMA', 132, '15');
-INSERT INTO Sells values('Netto', 133, '20');
-INSERT INTO Sells values('Netto', 124, '15');
-INSERT INTO Sells values('Fotex', 128, '20');
-INSERT INTO Sells values('REMA', 127, '15');
-INSERT INTO Sells values('Bilka', 128, '20');
-INSERT INTO Sells values('REMA', 135, '15');
-INSERT INTO Sells values('Fakta', 137, '20');
-INSERT INTO Sells values('Fakta', 138, '15');
-INSERT INTO Sells values('Fakta', 138, '20');
-INSERT INTO Sells values('Fakta', 127, '15');
-INSERT INTO Sells values('Fakta', 130, '20');
-INSERT INTO Sells values('Fotex', 130, '15');
-INSERT INTO Sells values('Fotex', 136, '20');
-INSERT INTO Sells values('Fotex', 127, '15');
-INSERT INTO Sells values('Fotex', 128, '20');
-INSERT INTO Sells values('REMA', 127, '15');
-INSERT INTO Sells values('Fotex', 139, '20');
-INSERT INTO Sells values('Bilka', 139, '15');
-INSERT INTO Sells values('Bilka', 138, '20');
-INSERT INTO Sells values('Bilka', 137, '15');
-INSERT INTO Sells values('Bilka', 136, '20');
-INSERT INTO Sells values('Bilka', 127, '15');
-INSERT INTO Sells values('Fotex', 136, '20');
-INSERT INTO Sells values('REMA', 129, '15');
-INSERT INTO Sells values('Fotex', 128, '20');
-INSERT INTO Sells values('Netto', 129, '15');
-INSERT INTO Sells values('Fotex', 129, '20');
-INSERT INTO Sells values('REMA', 127, '15');
-INSERT INTO Sells values('Netto', 128, '20');
-INSERT INTO Sells values('REMA', 125, '15');
-INSERT INTO Sells values('Fotex', 124, '20');
-INSERT INTO Sells values('Netto', 126, '15');
-INSERT INTO Sells values('Netto', 126, '20');
-INSERT INTO Sells values('Netto', 126, '15');
-INSERT INTO Sells values('Fotex', 135, '20');
-INSERT INTO Sells values('Fotex', 135, '15');
+INSERT INTO sells values('REMA', 122, '10');
+INSERT INTO sells values('Fotex', 121, '15');
+INSERT INTO sells values('REMA', 132, '20');
+INSERT INTO sells values('Fotex', 122, '25');
+INSERT INTO sells values('REMA', 123, '30');
+INSERT INTO sells values('Fotex', 123, '40');
+INSERT INTO sells values('Bilka', 121, '38');
+INSERT INTO sells values('Netto', 121, '20');
+INSERT INTO sells values('REMA', 124, '100');
+INSERT INTO sells values('Fotex', 124, '80');
+INSERT INTO sells values('REMA', 125, '5');
+INSERT INTO sells values('Netto', 122, '5'); -- cheapest drink
+INSERT INTO sells values('Netto', 123, '60');
+INSERT INTO sells values('Fotex', 125, '200');
+INSERT INTO sells values('REMA', 126, '85');
+INSERT INTO sells values('Netto', 124, '60');
+INSERT INTO sells values('Netto', 125, '65');
+INSERT INTO sells values('Fotex', 126, '87');
+INSERT INTO sells values('REMA', 127, '15');
+INSERT INTO sells values('Bilka', 122, '20');
+INSERT INTO sells values('REMA', 128, '15');
+INSERT INTO sells values('Fakta', 121, '20');
+INSERT INTO sells values('Fakta', 122, '15');
+INSERT INTO sells values('Fakta', 123, '4');
+INSERT INTO sells values('Fakta', 124, '15');
+INSERT INTO sells values('Fakta', 140, '49'); -- FAKTA latte large, 49 dkk, horsens
+INSERT INTO sells values('Fotex', 127, '15');
+INSERT INTO sells values('Fotex', 128, '20');
+INSERT INTO sells values('Fotex', 129, '15');
+INSERT INTO sells values('Fotex', 130, '20');
+INSERT INTO sells values('REMA', 129, '15');
+INSERT INTO sells values('Fotex', 131, '20');
+INSERT INTO sells values('Bilka', 123, '15');
+INSERT INTO sells values('Bilka', 124, '20');
+INSERT INTO sells values('Bilka', 125, '15');
+INSERT INTO sells values('Bilka', 126, '20');
+INSERT INTO sells values('Bilka', 140, '35'); -- bilka, large latte, 35 dkk, randers
+INSERT INTO sells values('Fotex', 132, '10');
+INSERT INTO sells values('REMA', 130, '15');
+INSERT INTO sells values('Fotex', 133, '20');
+INSERT INTO sells values('Netto', 126, '15');
+INSERT INTO sells values('Fotex', 134, '20');
+INSERT INTO sells values('REMA', 131, '15');
+INSERT INTO sells values('Netto', 127, '20');
+INSERT INTO sells values('REMA', 140, '40'); -- rema large latte, 40 dkk, horsens
+INSERT INTO sells values('Fotex', 135, '20');
+INSERT INTO sells values('Netto', 128, '15');
+INSERT INTO sells values('Netto', 129, '20');
+INSERT INTO sells values('Netto', 130, '15');
+INSERT INTO sells values('Fotex', 136, '20');
+INSERT INTO sells values('Fotex', 137, '15');
--- Assignment_2/assignment_2.sql
+++ Assignment_2/assignment_2.sql
@@ -14,23 +14,59 @@ ORDER BY A.name;
 -- 4. Find the names and the phone numbers of the stores in "Randers" or "Horsens" that sell a 'maxi' beverage named "latte"
 -- for no more than 45 kr
 
-select `name`, telephone
-from store
-where location in ('Randers', 'Horsens') and 
+select a.`name`, a.telephone
+from store a, sells b, beverage c
+where a.location in ('Randers', 'Horsens') 
+	and b.`code` = c.`code` 
+	and b.store_name = a.`name` 
+	and b.price <= 45 and c.`name` = 'latte' and c.size = 'large';
 
 -- 5. Find the code(s), name(s), and name(s) of store(s), selling the least expensive beverage(s).
+select c.`code`, c.`name`, a.`name`
+from store a, sells b, beverage c
+WHERE 
+	b.`code` = c.`code` 
+	and b.store_name = a.`name` 
+    and b.price = (
+        SELECT 
+            MIN(sells.price)
+        FROM
+            sells);
 
 -- 6.For each store , give its name and the code(s) of the least expensive beverage(s) it sells.
-
+select a.store_name, a.`code`, a.price
+from sells a, store b, beverage c
+group by a.`store_name`, a.price
+having (a.price, a.store_name) in (select min(t.price), t.store_name from sells t, store g where t.`store_name` = g.`name` group by t.`store_name`)
+order by a.price;
 
 -- 7. For each store, give its name and the price of the least expensive beverage(s) it sells.
 -- Repeat, but now (i) include the name(s) of such beverage(s) and (ii) do not use any aggregation operations,
 -- such as like MIN, GROUP BY, ORDER BY, etc.
 
+	-- 7.1
+select a.store_name, a.price
+from sells a, store b, beverage c
+group by a.`store_name`, a.price
+having (a.price, a.store_name) in (select min(t.price), t.store_name from sells t, store g where t.`store_name` = g.`name` group by t.`store_name`)
+order by a.price asc;
+
+	-- 7.2
+select a.`name`, c.`name`, b.price
+from store a, sells b, beverage c
+	inner join 
+
 -- 8. For each beverage, give its name, size, and its highest price across all stores. 
 -- Repeat, but now (i) include the name(s) of store(s) selling that beverage at the highest price, and (ii) 
 -- do not use any aggregation operations,such as MAX, GROUP BY, ORDER BY, etc.
 
+select c.`name`, c.size, a.price
+from beverage b, sells s
+group by a.`store_name`, a.price, c.size
+having (a.price, c.size, a.store_name) in (select max(t.price), t.store_name, i.size from sells t, store g, beverage i where t.`store_name` = g.`name` group by t.`store_name`)
+order by a.price desc;
+
 -- 9. Find the names of the stores that offer all beverage codes; do not use COUNT.
 
--- 10. Find the largest price difference between beverages of the same name across all stores, the name(s) of the beverage(s) having that price difference, and the relevant store(s).
+-- 10. Find the largest price difference between beverages of the same name across all stores, the name(s) of the beverage(s)
+-- having that price difference, and the relevant store(s).
